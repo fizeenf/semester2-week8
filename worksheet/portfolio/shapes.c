0cf732fcd4537885bfe1b5aca8f5c91ec29554d0
fizeenf <lhxz0885@leeds.ac.uk>
2026-03-24 18:36:16 +0000

    saved

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -5,10 +5,26 @@
 #include "shapes.h"
 
 Point makePoint( float x, float y ) {
-    Point new;
-    // implementation
-    return new;
+    Point pnt;
+    pnt.x = x, pnt.y = y;
+    return pnt;
 }
+
+Line makeLine(Point pnt1, Point pnt2){
+    Line ln;
+    ln.p[0] = pnt1, ln.p[1] = pnt2;
+    return ln;
+    
+}
+
+Triangle makeTriangle(Point pnt1, Point pnt2, Point pnt3){
+    Triangle Trn;
+    Trn.p[0] = pnt1, Trn.p[1] = pnt2, Trn.p[2] = pnt3;
+    return Trn;
+}
+
+
+
 
 // complete other functions below
 // - start with stubs as above
